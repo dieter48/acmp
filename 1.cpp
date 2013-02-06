ef66dbf1bb256881522e4934aa2419fc360510af
Dieter Munz <chekist48@gmail.com>
2013-02-06 21:34:57 +0800

    The source code of the task № 1

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+/*
+Task: "A+B";
+Link: "http://acmp.ru/index.asp?main=task&id_task=1";
+Author: "https://github.com/dieter48;
+*/
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+int a,b;
+cin>>a>>b;
+cout<<a+b;
+}
