a0e1abe11b9df1902cd6826862b183f2ccfa7258
Dieter Munz <chekist48@gmail.com>
2013-02-08 22:18:43 +0800

    Fixed error
    Fixed date format DD / MM / YYYY

--- 550.cpp
+++ 550.cpp
@@ -9,8 +9,11 @@ using namespace std;
 int main(){
 freopen("input.txt","r",stdin);
 freopen("output.txt","w",stdout);
-int n,sw;
+int n;
 cin>>n;
-if((n%4==0)&&(n%100!=0)||(n%400==0)) sw=12; else sw=13;
-cout<<sw<<"/09/"<<n;
+if((n%4==0)&&(n%100!=0)||(n%400==0)) cout<<"12"<<"/09/"; else cout<<"13"<<"/09/";
+if(n<10) cout<<"0";
+if(n<100) cout<<"0";
+if(n<1000) cout<<"0";
+cout<<n;
 }
