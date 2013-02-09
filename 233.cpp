e85a8a2d56d127db43047c597175d6c5e4ef98a0
Dieter Munz <chekist48@gmail.com>
2013-02-09 23:13:53 +0800

    Some simple tasks

---
+++ 233.cpp
@@ -0,0 +1,25 @@
+/*
+Task: "Avtobysnaya ekskyrsiya";
+Link: "http://acmp.ru/index.asp?main=task&id_task=233";
+Author: "https://github.com/dieter48;
+*/
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+int n,f,i,c=0;
+cin>>n;
+for(i=1;i<=n;i++)
+{
+    cin>>f;
+    if(f<=437)
+    {
+        cout << "Crash " << i;
+        c++;
+        break;
+    }
+}
+    if(c==0) cout << "No crash";
+}
---
+++ 81.cpp
@@ -0,0 +1,21 @@
+/*
+Task: "Arbyzi";
+Link: "http://acmp.ru/index.asp?main=task&id_task=81";
+Author: "https://github.com/dieter48;
+*/
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+int n,x,max=0,min=32000,i;
+cin>>n;
+for(i=1;i<=n;i++)
+{
+    cin>>x;
+    if(x>max) max=x;
+    if(x<min) min=x;
+}
+cout<<min<<" "<<max;
+}
