c1c966820d22548e47c7cfb32298c3b3de3fd1ce
Dieter Munz <chekist48@gmail.com>
2013-02-11 01:11:27 +0800

    Simple task

---
+++ 327.cpp
@@ -0,0 +1,25 @@
+/*
+Task: "V odnom shage ot shast'ya";
+Link: "http://acmp.ru/index.asp?main=task&id_task=327";
+Author: "https://github.com/dieter48;
+*/
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+bool ch(int a)
+{
+    if((a/100000+a/10000%10+a/1000%10)!=(a/100%10+a/10%10+a%10)) return false;
+    return true;
+}
+int main(){
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+int n,i,f;
+cin>>n;
+for(i=0;i<n;i++)
+{
+    cin>>f;
+    if(ch(f+1)||ch(f-1)) cout<<"Yes"; else cout<<"No";
+    cout<<endl;
+}
+}
