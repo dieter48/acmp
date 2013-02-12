724e383702f28dd7c1c3239e3521cd8f0ab02a74
Dieter Munz <chekist48@gmail.com>
2013-02-12 19:16:50 +0800

    Removed getchar()

--- 297.cpp
+++ 297.cpp
@@ -11,7 +11,7 @@ freopen("input.txt","r",stdin);
 freopen("output.txt","w",stdout);
 char c;
 int i=0;
-while((c=getchar())!=EOF)
+while(cin>>c)
     if(c=='0'||c=='6'||c=='9') i++;
     else if(c=='8') i+=2;
 cout<<i;
